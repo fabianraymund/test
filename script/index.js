0ab41142e83f734c498c0d2254908909df5c27fd
DannyPreye <99923947+DannyPreye@users.noreply.github.com>
2022-03-30 16:13:57 +0100

    added new files 30/3/22

--- script/index.js
+++ script/index.js
@@ -1,12 +1,37 @@
 // Dom elements
-
-
-
+const section1 = document.getElementById('section1')
+const sections = document.querySelectorAll('section')
 
 // Function
 function scroll(param) {
     const options={
-        
+        threshold: 0,
+        rootMargin: '0px'
     }
-    const observer = new IntersectionObserver()
+    const observer = new IntersectionObserver(function(entries,observer){
+        entries.forEach(entry=>{
+            if(entry.isIntersecting==true){
+                entry.target.classList.add("second")
+            }
+            else{
+                entry.target.classList.remove("second")
+            }
+        })
+    },options)
+
+  return  observer.observe(param);
 }
+
+
+
+sections.forEach(section=>{
+    const alldiv = section.querySelectorAll('div');
+    alldiv.forEach(div=>{
+        if(!div.classList.contains('no')){
+            div.classList.add('first')
+        }
+    
+        scroll(div);
+    })
+   
+})
